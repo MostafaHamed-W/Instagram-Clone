02518767a2ea87fcb0fa2503e31c2c90a6122bc7
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-05-26 15:44:47 +0300

    Web Setup With Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive.dart';
@@ -7,7 +8,18 @@ import 'package:instagram_clone/utilities/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  if (kIsWeb) {
+    Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyBdYKquAKQ5_QTiEhC13D3qGvN-QELcChY",
+      appId: "1:923956321064:web:fe36b77a2f68b7cd9cce1d",
+      messagingSenderId: "923956321064",
+      projectId: "instagram-clone-87631",
+      storageBucket: "instagram-clone-87631.appspot.com",
+    ));
+  } else {
+    Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
