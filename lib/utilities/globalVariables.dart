9b29a4d2f2a7d3bdc7a4203d1c201cdc3aa6aec8
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-06-04 10:32:27 +0300

    Updating Post Card

--- lib/utilities/globalVariables.dart
+++ lib/utilities/globalVariables.dart
@@ -30,6 +30,7 @@ class PostCard extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16).copyWith(right: 0),
             child: Row(
               children: [
+                // Header Section
                 const CircleAvatar(
                   radius: 16,
                   backgroundImage: NetworkImage(
@@ -51,10 +52,44 @@ class PostCard extends StatelessWidget {
                     ),
                   ),
                 ),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+                IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                          child: ListView(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shrinkWrap: true,
+                              children: [
+                                'Delete',
+                              ]
+                                  .map(
+                                    (e) => InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12, horizontal: 16),
+                                        child: Text(e),
+                                      ),
+                                    ),
+                                  )
+                                  .toList()),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.more_vert))
               ],
             ),
           ),
+          // Header Section
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.37,
+            width: double.infinity,
+            child: Image.network(
+              'https://images.unsplash.com/photo-1556189250-72ba954cfc2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+              fit: BoxFit.cover,
+            ),
+          )
         ],
       ),
     );
