6656e8123b4e95e7b468527da170adb9efcda266
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-06-12 12:33:04 +0300

    Adding Like Feature & Like Animation

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utilities/colors.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:instagram_clone/utilities/globalVariables.dart';
+
+import '../widgets/post_card.dart';
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/resources/firestore_methods.dart';
+import 'package:instagram_clone/utilities/colors.dart';
+import 'package:instagram_clone/widgets/like_animation.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../model/user.dart';
+import 'package:instagram_clone/providers/provider.dart';
+
+class PostCard extends StatefulWidget {
+  final snap;
+  const PostCard({super.key, required this.snap});
+
+  @override
+  State<PostCard> createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  bool isLikeAnimating = false;
+  @override
+  Widget build(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser;
+    bool isBookmarked = false;
+
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16).copyWith(right: 0),
+            child: Row(
+              children: [
+                // Header Section
+                //user profile pic
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(
+                    widget.snap['profImg'],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.snap['username'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("Tanta")
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                          child: ListView(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shrinkWrap: true,
+                              children: [
+                                'Delete',
+                              ]
+                                  .map(
+                                    (e) => InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12, horizontal: 16),
+                                        child: Text(e),
+                                      ),
+                                    ),
+                                  )
+                                  .toList()),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.more_horiz))
+              ],
+            ),
+          ),
+          // Header Section
+          // post image
+
+          GestureDetector(
+            onDoubleTap: () async {
+              await FirestoreMethods().likePost(
+                postId: widget.snap['postId'],
+                uid: widget.snap['uid'],
+                likes: widget.snap['likes'],
+              );
+              setState(() {
+                isLikeAnimating = true;
+              });
+            },
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.45,
+                  width: double.infinity,
+                  child: Image.network(
+                    widget.snap['postUrl'],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                AnimatedOpacity(
+                  duration: const Duration(milliseconds: 200),
+                  opacity: isLikeAnimating ? 1 : 0,
+                  child: LikeAnimation(
+                    isAnimating: isLikeAnimating,
+                    duration: const Duration(milliseconds: 200),
+                    onEnd: () {
+                      setState(() {
+                        isLikeAnimating = false;
+                      });
+                    },
+                    child: const Icon(
+                      Icons.favorite,
+                      size: 120,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Like and comment and share section
+          Row(
+            children: [
+              LikeAnimation(
+                isAnimating: widget.snap['likes'].contains(user.uid),
+                smallLike: true,
+                child: IconButton(
+                  onPressed: () async {
+                    await FirestoreMethods().likePost(
+                        postId: widget.snap['postId'], uid: user.uid, likes: widget.snap['likes']);
+                  },
+                  icon: widget.snap['likes'].contains(user.uid)
+                      ? const FaIcon(
+                          FontAwesomeIcons.solidHeart,
+                          color: Colors.red,
+                        )
+                      : const FaIcon(FontAwesomeIcons.heart),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.comment,
+                ),
+              ),
+              Transform.rotate(
+                angle: -0.8,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const FaIcon(
+                    Icons.send,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      isBookmarked == true
+                          ? FontAwesomeIcons.solidBookmark
+                          : FontAwesomeIcons.bookmark,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Descrition and number of likes
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style:
+                      Theme.of(context).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w900),
+                  child: Text(
+                    "${widget.snap["likes"].length} likes",
+                    // "1,250 likes",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 8),
+                  width: double.infinity,
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(
+                        color: primaryColor,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: widget.snap['username'],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        const TextSpan(text: "  "),
+                        TextSpan(
+                          text: widget.snap['description'],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: const Text(
+                      "View all 200 comments",
+                      style: TextStyle(
+                        color: secondaryColor,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  DateFormat.yMMMd().format(widget.snap['datePublished'].toDate()),
+                  style: const TextStyle(fontSize: 15),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
