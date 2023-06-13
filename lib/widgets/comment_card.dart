d8b9ba8439539371ccf963820d748ce6a283a550
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-06-13 11:57:57 +0300

    Some Edits

--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/provider.dart';
 import 'package:intl/intl.dart';
-
+import 'package:provider/provider.dart';
+import '../model/user.dart';
 import '../utilities/colors.dart';
 
 class CommentCard extends StatefulWidget {
@@ -15,8 +17,58 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
+    User user = Provider.of<UserProvider>(context).getUser;
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 18,
+            backgroundImage: NetworkImage(user.photoUrl),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: user.username,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const TextSpan(
+                          text: " some description to insert",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 4),
+                    child: Text(
+                      "13/06/2023",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: secondaryColor,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Icon(
+              Icons.favorite,
+              size: 16,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
